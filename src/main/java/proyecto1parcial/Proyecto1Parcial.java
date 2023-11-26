d630efeff3748d68614972e7a50b30fa0c839a0b
Steven <Steven@StevenFue>
2023-11-25 19:47:55 -0500

    Segundo commit

---
+++ src/main/java/proyecto1parcial/Proyecto1Parcial.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto1parcial;
+
+/**
+ *
+ * @author Steven
+ */
+public class Proyecto1Parcial {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
