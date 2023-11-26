70f04fe473d3a1fb54a5b08264780688128b9c05
Steven <Steven@StevenFue>
2023-11-25 20:04:08 -0500

    Creacion del main

---
+++ src/main/java/proyecto1parcial/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto1parcial;
+
+/**
+ *
+ * @author Steven
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
