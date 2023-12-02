84bbe3b43fa05c1438f656b178e262cbc73a5152
Steven <Steven@StevenFue>
2023-12-02 01:11:16 -0500

    Metodos ferias agregadas y ejecucion

--- src/main/java/modelo/evento/SeccionStand.java
+++ src/main/java/modelo/evento/SeccionStand.java
@@ -31,5 +31,5 @@ public class SeccionStand {
         }
     }
     
-  
+   
 }
--- src/main/java/modelo/participante/Emprendedor.java
+++ src/main/java/modelo/participante/Emprendedor.java
@@ -4,8 +4,8 @@ public class Emprendedor extends Persona{
     private String desServicio;
     
     //Constructor
-    public Emprendedor(String ni, String n, String npr, String t, String e, String d, String sw, String ds){
-        super(ni,n,npr,t,e,d,sw);
+    public Emprendedor(String ni, String n, String npr, int t, String e, String d, String sw, String ds){
+        super(ni,n,t,e);
         desServicio=ds;
     }
     
