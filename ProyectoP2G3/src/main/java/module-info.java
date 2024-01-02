e0dccaacd9f3e936d0688fbfa93cf42b415c3135
Marcia Torres G. <matogarc@espol.edu.ec>
2024-01-01 20:48:08 -0500

    Modifiqué algunas cosas para empezar a crear la interfaz.

--- ProyectoP2G3/src/main/java/module-info.java
+++ ProyectoP2G3/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.mycompany.proyectop2g3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.proyectop2g3 to javafx.fxml;
+    opens com.mycompany.sistema to javafx.fxml;
     exports com.mycompany.proyectop2g3;
 }
