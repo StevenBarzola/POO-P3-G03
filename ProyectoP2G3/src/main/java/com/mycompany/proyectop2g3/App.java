faf82d43e91046409ed560f73114f4d34dbbcb45
Marcia Torres G. <matogarc@espol.edu.ec>
2024-01-12 22:10:19 -0500

    Modifiqué algunas cosas para empezar a crear la interfaz.

--- ProyectoP2G3/src/main/java/com/mycompany/proyectop2g3/App.java
+++ ProyectoP2G3/src/main/java/com/mycompany/proyectop2g3/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("MenuFXML"), 640, 480);
+        scene = new Scene(loadFXML("sistema/MenuFXML"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
