76a0d61cedacc781d76d17566ebf13d64ce71350
USER <USER@DESKTOP-CHIMO1D>
2024-01-13 21:58:32 -0500

    se puede crear un auspiciante, está a medias el edit Auspiciante

--- ProyectoP2G3/src/main/java/com/mycompany/proyectop2g/EditarAuspiController.java
+++ ProyectoP2G3/src/main/java/com/mycompany/proyectop2g/EditarAuspiController.java
@@ -8,7 +8,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import java.io.*;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Alert;
+import modelo.participante.*;
+import modelo.evento.*;
 
 /**
  * FXML Controller class
@@ -20,9 +29,43 @@ public class EditarAuspiController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML
+    private TextField cedula;
+    @FXML
+    private VBox editarAuspiciante;
+    @FXML
+    private ComboBox<String> campos;
+    @FXML
+    private GridPane NombrePanel;
+    @FXML
+    private GridPane NombreRespoPanel;
+    @FXML
+    private GridPane TelefonoPanel;
+    @FXML
+    private GridPane EmailPanel;
+    @FXML
+    private GridPane DireccionPanel;
+    @FXML
+    private GridPane SitioWebPanel;
+    @FXML
+    private GridPane SectorCubiertoPanel;
+    @FXML
+    private GridPane RedSocialPanel;
+    @FXML
+    private StackPane ventanaPrincipal;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        campos.getItems().add("Nombre");
+        campos.getItems().add("Nombre P.Responsable");
+        campos.getItems().add("Telefono");
+        campos.getItems().add("Email");
+        campos.getItems().add("Direccion");
+        campos.getItems().add("Sitio Web");
+        campos.getItems().add("Sector Cubierto");
+        campos.getItems().add("Red Social");
+        editarAuspiciante.setVisible(false);
     }   
     
     @FXML
@@ -30,4 +73,61 @@ public class EditarAuspiController implements Initializable {
         App.setRoot("/com/mycompany/proyectop2g/admAuspiciante");
     }
     
+    @FXML
+    private void verificarAuspiciante(){
+        if(Auspiciante.verificarCedula(cedula.getText())){
+            Persona perEdit = Sistema.encontrarPersona(cedula.getText());
+            if(perEdit!=null && perEdit instanceof Auspiciante){
+                     Auspiciante auspiEditar=(Auspiciante)perEdit;
+            }
+            editarAuspiciante.setVisible(true);
+            campos.setOnAction(
+                     e -> {
+                        NombrePanel.setVisible(false);
+                        NombreRespoPanel.setVisible(false);
+                        TelefonoPanel.setVisible(false);
+                        EmailPanel.setVisible(false);
+                        DireccionPanel.setVisible(false);
+                        SitioWebPanel.setVisible(false);
+                        SectorCubiertoPanel.setVisible(false);
+                        RedSocialPanel.setVisible(false);
+                        String casos = campos.getValue();
+                        switch(casos){
+                             case "Nombre" :
+                                NombrePanel.setVisible(true);
+                                break;
+                            case "Nombre P.Responsable":
+                                NombreRespoPanel.setVisible(true);
+                                break;
+                            case "Telefono":
+                                TelefonoPanel.setVisible(true);
+                                break;
+                            case "Email":
+                                EmailPanel.setVisible(true);
+                                break;
+                            case "Direccion":
+                                DireccionPanel.setVisible(true);
+                                break;
+                            case "Sitio Web":
+                                SitioWebPanel.setVisible(true);
+                                break;
+                            case "Sector Cubierto":
+                                SectorCubiertoPanel.setVisible(true);
+                                break;
+                            case "Red Social":
+                                RedSocialPanel.setVisible(true);
+                                break;
+                        }
+                     }
+            );
+        }else{
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Usuario no Encontrado");
+            alert.setContentText("No se encontrado un auspiciante con la cedula ingresada.");
+            alert.showAndWait();
+        }
+    }
+    
+
 }
