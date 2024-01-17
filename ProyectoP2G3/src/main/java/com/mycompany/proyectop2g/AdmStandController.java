1cc9951a1b301e0902dcf6fcaec1e48d9d438bc1
USER <USER@DESKTOP-CHIMO1D>
2024-01-16 20:42:31 -0500

    diseñoo

--- ProyectoP2G3/src/main/java/com/mycompany/proyectop2g/AdmStandController.java
+++ ProyectoP2G3/src/main/java/com/mycompany/proyectop2g/AdmStandController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,11 +14,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import modelo.evento.*;
 import modelo.participante.*;
 /**
@@ -40,6 +43,15 @@ public class AdmStandController implements Initializable {
     private HBox hb4Seccion;
     
     private Feria feria;
+    @FXML
+    private HBox ventanaRegistrarStand;
+    
+     @FXML
+    private TextField txtIDPersona;
+    
+    //private Feria feria;
+    private Stand stand;
+    private Label lb; //Para cerrar la escena de su ventana
     /**
      * Initializes the controller class.
      */
@@ -97,7 +109,10 @@ public class AdmStandController implements Initializable {
                        alerta.getButtonTypes().setAll(botonSi, botonNo);
                        alerta.showAndWait().ifPresent(response -> {
                        if (response == botonSi) {
-                           try{
+                           iniAtributos(feria,s,lblFeriaSeleccionada);
+                           ventanaRegistrarStand.setVisible(true);
+                           /*
+                           try{                               
                                FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/mycompany/proyectop2g/reservarStand.fxml"));
                                Parent root= loader.load();
                                ReservarStandController controlador= loader.getController();
@@ -107,10 +122,12 @@ public class AdmStandController implements Initializable {
                                stage.initModality(Modality.APPLICATION_MODAL);
                                stage.setScene(scene);
                                stage.showAndWait();
+                                
                                
                            }catch(IOException io){
                                 System.out.println("Error cambio de ventana: "+io.getMessage());
                            }
+                            */
                        }else if(response == botonNo) {
                            alerta.close();
                        }
@@ -129,11 +146,142 @@ public class AdmStandController implements Initializable {
     @FXML
     private void regresar(MouseEvent event) throws IOException{
         feria=null;
+        stand=null;
         App.setRoot("/com/mycompany/proyectop2g/admFeria");
         Stage stage= (Stage) lblFeriaSeleccionada.getScene().getWindow();
         stage.close();
     }
     
+    
+    public void iniAtributos(Feria fe, Stand st, Label lbl){
+        feria=fe;
+        stand=st;
+        lb=lbl;
+    }
+    
+    @FXML
+    private void reservar(ActionEvent event) throws IOException{
+        Persona per= Sistema.encontrarPersona(txtIDPersona.getText());
+        int cont=0;
+        if(per!=null && per instanceof Emprendedor){
+            Emprendedor emp=(Emprendedor)per;
+            cont= feria.limiteStand(emp.getNumId());
+            if(cont==0){
+                stand.setPersona(emp);
+                stand.setFechaAsig(LocalDate.now());
+                Alert alerta= new Alert(Alert.AlertType.INFORMATION);
+                alerta.setTitle("Aviso");
+                alerta.setHeaderText("Stand "+stand.getCodigoSt());
+                alerta.setContentText("El stand ha sido reservado con exito");
+                alerta.showAndWait();
+                feria=null;
+                stand=null;
+                //Stage stage= (Stage) txtIDPersona.getScene().getWindow();
+                //stage.close();
+                //abajo
+                //Stage stage2= (Stage) lb.getScene().getWindow();
+                //stage2.close();
+                App.setRoot("/com/mycompany/proyectop2g/admFeria");
+                
+                FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/mycompany/proyectop2g/admStand.fxml"));
+                Parent root= loader.load();
+                AdmStandController controlador= loader.getController();
+                controlador.crearStands(feria);
+                Scene scene= new Scene(root);
+                Stage stage1= new Stage();
+                stage1.initModality(Modality.APPLICATION_MODAL);
+                stage1.setScene(scene);
+                stage1.initStyle(StageStyle.UNDECORATED);
+                feria=null;
+                stand=null;
+                ventanaRegistrarStand.setVisible(false);
+                stage1.show();
+                //arriba
+            }else{
+                Alert alerta= new Alert(Alert.AlertType.WARNING);
+                alerta.setTitle("Aviso");
+                alerta.setHeaderText("El emprendedor ya tiene un stand asignado");
+                alerta.setContentText("Un emprendedor no puede tener mas de un stand asignado por feria");
+                alerta.showAndWait();
+            } 
+        }else if(per!=null && per instanceof Auspiciante){
+            Auspiciante aus=(Auspiciante)per;
+            AuspicianteEnFeria anf= feria.encontrarAuspicianteEnFeria(aus);
+            if(anf!=null){
+                cont= feria.limiteStand(anf.getAuspiciante().getNumId());
+                if(cont<2){
+                    if(anf.getTieneStand()==true){
+                        stand.setPersona(aus);
+                        stand.setFechaAsig(LocalDate.now());
+                        Alert alerta= new Alert(Alert.AlertType.INFORMATION);
+                        alerta.setTitle("Aviso");
+                        alerta.setHeaderText("Stand "+stand.getCodigoSt());
+                        alerta.setContentText("El stand ha sido reservado con exito");
+                        alerta.showAndWait();
+                        //Stage stage= (Stage) txtIDPersona.getScene().getWindow();
+                        //stage.close();
+                        //abajo
+                        //Stage stage2= (Stage) lb.getScene().getWindow();
+                        //stage2.close();
+                        App.setRoot("/com/mycompany/proyectop2g/admFeria");
+                        
+                        FXMLLoader loader= new FXMLLoader(getClass().getResource("/com/mycompany/proyectop2g/admStand.fxml"));
+                        Parent root= loader.load();
+                        AdmStandController controlador= loader.getController();
+                        controlador.crearStands(feria);
+                        Scene scene= new Scene(root);
+                        Stage stage1= new Stage();
+                        stage1.initModality(Modality.APPLICATION_MODAL);
+                        stage1.setScene(scene);
+                        stage1.initStyle(StageStyle.UNDECORATED);
+                        
+                        crearStands(feria);
+                        feria=null;
+                        stand=null;
+                        ventanaRegistrarStand.setVisible(false);
+                        Stage stage= (Stage) lblFeriaSeleccionada.getScene().getWindow();
+                        stage.close();
+                        stage1.show();
+                        
+                        //arriba
+                    }else{
+                        Alert alerta= new Alert(Alert.AlertType.WARNING);
+                        alerta.setTitle("Aviso");
+                        alerta.setHeaderText("El auspiciante no debe tener stand");
+                        alerta.setContentText("El auspiciante que ingreso no va a requerir el uso de un stand");
+                        alerta.showAndWait();
+                    } 
+                }else{
+                    Alert alerta= new Alert(Alert.AlertType.WARNING);
+                    alerta.setTitle("Aviso");
+                    alerta.setHeaderText("El auspiciante ya tiene 2 stands asignados");
+                    alerta.setContentText("Un auspiciante solo puede tener max 2 stands por feria");
+                    alerta.showAndWait();
+                }
+            }else{
+                Alert alerta= new Alert(Alert.AlertType.ERROR);
+                alerta.setTitle("Aviso");
+                alerta.setHeaderText("Emprendedor o Auspiciante no encontrado");
+                alerta.setContentText("Asegurese que la persona se haya registrado y que el numero de identificacion este correctamente escrito");
+                alerta.showAndWait();
+            }    
+        }else{
+            Alert alerta= new Alert(Alert.AlertType.ERROR);
+            alerta.setTitle("Aviso");
+            alerta.setHeaderText("Emprendedor o Auspiciante no encontrado");
+            alerta.setContentText("Asegurese que la persona se haya registrado y que el numero de identificacion este correctamente escrito");
+            alerta.showAndWait();
+        }
+    }
+    
+    
+    @FXML
+    private void regresar2(MouseEvent event) throws IOException{
+        feria=null;
+        stand=null;
+        Stage stage1= (Stage) txtIDPersona.getScene().getWindow();
+        stage1.close();
+    }
     
     
 }
